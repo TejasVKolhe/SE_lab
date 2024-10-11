c6099c0255d6189a4c17e07a3473616175367ccc
khedkarsayali <sayalikhedkar6@gmail.com>
2024-10-11 15:39:43 +0530

    Added calculator code

--- se.sh
+++ se.sh
@@ -1,2 +1,19 @@
 "!bin/bash
-echo "hi"
+
+echo "Enter first number: "
+read a
+echo "Enter second number: "
+read b
+
+echo "Enter operation: +, -, *, /"
+read op
+
+case $op in
+    "+") result=$((a + b)) ;;
+    "-") result=$((a - b)) ;;
+    "/") result=$((a * b)) ;;
+    "*") result=$((a / b)) ;;
+    *) echo "Invalid operation!" ;;
+esac
+
+echo "Result: $result"
